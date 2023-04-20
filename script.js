5fa92c284e5be411a8a465823d08cf2a35fb5b94
DomenicoLucarelli <domyluca@gmail.com>
2023-04-20 19:28:08 +0200

    bonus che cancella gli elementi della lista

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const { createApp } = Vue
     methods: {
         getJson(){
             // faccio una chiamata ad axios per ottenere l'oggetto json
-            axios.get('./todos.json').then(res=>{
+            axios.get('./server.php').then(res=>{
                 
                 // metto l'oggetto jason nell'array todos
                 this.todos = res.data;
@@ -27,18 +27,48 @@ const { createApp } = Vue
         pushNewEl(){
 
             let data = {
-                newEl : '',
+                newEl : ''
             }
 
             data.newEl = this.newEl ;
 
             axios.post('./server.php', data, {headers: {'Content-Type': 'multipart/form-data'}}).then(response => {
                
+                
                 this.getJson();
 
             });
 
             this.newEl = ''
+        },
+
+        // funzione che barra l'elemento
+        strike(todo , indice){
+            let data = {
+                newEl : todo.text,
+                elIndex : indice ,
+                boolean : todo.isClick
+
+            }
+
+            console.log(data)
+            axios.post('./server.php', data, {headers: {'Content-Type': 'multipart/form-data'}}).then(response => {
+
+                this.getJson();
+            
+            });
+        },
+
+        cancella(indice){
+            let data = {
+                index : indice
+            }
+
+            axios.post('./server.php', data, {headers: {'Content-Type': 'multipart/form-data'}}).then(response => {
+
+                this.getJson();
+            
+            });
         }
     },
 
--- server.php
+++ server.php
@@ -9,7 +9,12 @@ if(isset($_POST['newEl'])){
     $objectPhp = json_decode($objectJson);
 
     // pusho il nuovo elemento che ho fatto inserie all'utente
-    $objectPhp[] = $_POST['newEl'];
+    $objectPhp[] =[
+        
+        "text" => $_POST['newEl'],
+        "isClick" => false,
+    ] ;
+    
 
     // ritrasformo in oggetto json
     $newObjectJson = json_encode($objectPhp);
@@ -17,6 +22,17 @@ if(isset($_POST['newEl'])){
     // lo salvo nel file json
     file_put_contents('todos.json', $newObjectJson);
 
+}elseif(isset($_POST['index'])){
+
+    $objectJson = file_get_contents('todos.json');
+
+    $objectPhp = json_decode($objectJson);
+
+    array_splice($objectPhp, $_POST['index'], 1);
+
+    $newObjectJson = json_encode($objectPhp);
+  
+    file_put_contents('todos.json', $newObjectJson);
 
 }else{
 
@@ -30,7 +46,8 @@ if(isset($_POST['newEl'])){
     header('Content-type: application/json');
     
     // stampo l'oggetto una volta che l'ho ritrasformato in oggetto json
-    
     echo json_encode($objectPhp);
 
 }
+
+
